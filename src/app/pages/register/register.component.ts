fa0538c0e13deda9d1b13e347e96c718ede804b1
hoang92-gtvt <hoang92.gtvt@gmail.com>
2021-07-30 19:15:28 +0700

    form tao moi

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  formRegister : FormGroup = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+    email: new FormControl(),
+    phone: new FormControl(),
+  })
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  createAccount() {
+    console.log(this.formRegister.value);
+  }
 }
