d44d0dca3c34bbdf369a12955bb6cd09020b8e2e
ngtung-scoure <ngtung6395.com>
2021-08-09 01:00:09 +0700

    bug loi mat

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+let express = require('express');
+let app = express();
 
-const app = express();
-const http = require('http').createServer(app);
+let http = require('http');
+let server = http.Server(app);
 
-const io = require('socket.io')(http,{
-    cors:{
-        origin:'*'
-    }
-});
-
-app.get('/',(req,res)=>{
-    res.send('Hello world')
+app.get('/', (req, res) => {
+    res.send('Hello world');
 })
 
-let userList = new Map();
+let socketIO = require('socket.io');
+let io = socketIO(server);
 
+const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
-    let userName = socket.handshake.query.userName;
-    addUser(userName, socket.id);
+    socket.on('join', (data) => {
+        socket.join(data.room);
+        socket.broadcast.to(data.room).emit('user joined');
+    });
 
-    socket.broadcast.emit('user-list', [...userList.keys()]);
-    socket.emit('user-list', [...userList.keys()]);
-
-    socket.on('message', (msg) => {
-        socket.broadcast.emit('message-broadcast', {message: msg, userName: userName});
-    })
-
-    socket.on('disconnect', (reason) => {
-        removeUser(userName, socket.id);
-    })
+    socket.on('message', (data) => {
+        io.in(data.room).emit('new message', {user: data.user, message: data.message});
+    });
 });
 
-function addUser(userName, id){
-    if(!userList.has(userName)){
-        userList.set(userName, new Set(id));
-    }else{
-        userList.get(userName).add(id);
-    }
-}
-
-function removeUser(userName,id){
-    if(userList.has(userName)){
-        let userIds = userList.get(userName);
-        if(userIds.size ==0){
-            userList.delete(userName);
-        }
-    }
-};
-
-http.listen(3000, ()=>{
-    console.log('Server is running');
-})
+server.listen(port, () => {
+    console.log(`started on port: ${port}`);
+});
